a04845005119910e3582a46910961870e89ce067
Maciej Czajkowski <maciejpiotrczajkowski@gmail.com>
2021-02-03 23:27:11 +0100

    1.5.6 szkielet dokumentacji [javadoc]

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -3,6 +3,10 @@ package edu.kis.vh.nursery.factory;
 import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 
+/**
+ * Factory class for Rhymers objects
+ * @implements RhymersFactory
+ */
 public class DefaultRhymersFactory implements Rhymersfactory {
 
     @Override
@@ -16,6 +20,10 @@ public class DefaultRhymersFactory implements Rhymersfactory {
     }
 
     @Override
+    /**
+     * Factory getter for HanoiRhymer class object
+     * @return HanoiRhymer object
+     */
     public DefaultCountingOutRhymer getHanoiRhymer() {
         return new HanoiRhymer();
     }
